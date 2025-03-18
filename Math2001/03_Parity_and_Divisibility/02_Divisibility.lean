c1153d1cc97fa944d8681b3e18085ae4277742ce
Gabriel A <gabrielbritoapolinario@gmail.com>
2025-03-18 10:28:38 +0000

    complete 3.2

--- Math2001/03_Parity_and_Divisibility/02_Divisibility.lean
+++ Math2001/03_Parity_and_Divisibility/02_Divisibility.lean
@@ -60,7 +60,12 @@ example {a b : ℕ} (hb : 0 < b) (hab : a ∣ b) : a ≤ b := by
 
 
 example {a b : ℕ} (hab : a ∣ b) (hb : 0 < b) : 0 < a := by
-  sorry
+  obtain ⟨k, hk⟩ := hab
+  have h2 :=
+    calc
+    0 < b := hb
+    _ = a*k := hk
+  cancel k at h2
 
 /-! # Exercises -/
 
@@ -70,7 +75,11 @@ example (t : ℤ) : t ∣ 0 := by
   ring
 
 example : ¬(3 : ℤ) ∣ -10 := by
-  sorry
+  apply Int.not_dvd_of_exists_lt_and_lt
+  use -4
+  constructor
+  numbers
+  numbers
 
 example {x y : ℤ} (h : x ∣ y) : x ∣ 3 * y - 4 * y ^ 2 := by
   obtain ⟨k,hk⟩ := h
@@ -85,16 +94,37 @@ example {m n : ℤ} (h : m ∣ n) : m ∣ 2 * n ^ 3 + n := by
   ring
 
 example {a b : ℤ} (hab : a ∣ b) : a ∣ 2 * b ^ 3 - b ^ 2 + 3 * b := by
-  sorry
+  obtain ⟨k,hk⟩ := hab
+  use 2*a^2*k^3-a*k^2+3*k
+  rw[hk]
+  ring
 
 example {k l m : ℤ} (h1 : k ∣ l) (h2 : l ^ 3 ∣ m) : k ^ 3 ∣ m := by
-  sorry
+  obtain ⟨a,ha⟩ := h1
+  obtain ⟨b,hb⟩ := h2
+  use a^3*b
+  rw[hb,ha]
+  ring
 
 example {p q r : ℤ} (hpq : p ^ 3 ∣ q) (hqr : q ^ 2 ∣ r) : p ^ 6 ∣ r := by
-  sorry
+  obtain ⟨a,ha⟩ := hpq
+  obtain ⟨b,hb⟩ := hqr
+  use a^2*b
+  rw[hb,ha]
+  ring
 
 example : ∃ n : ℕ, 0 < n ∧ 9 ∣ 2 ^ n - 1 := by
-  sorry
+  use 6
+  constructor
+  numbers
+  use 7
+  numbers
 
 example : ∃ a b : ℤ, 0 < b ∧ b < a ∧ a - b ∣ a + b := by
-  sorry
+  use 3,1
+  constructor
+  numbers
+  constructor
+  numbers
+  use 2
+  numbers
