ceadd1c13364914ff91c024063808ecc196f6bbe
Gabriel A <gabrielbritoapolinario@gmail.com>
2025-03-19 07:52:39 +0000

    hw3

--- Math2001/Homework/hw3.lean
+++ Math2001/Homework/hw3.lean
@@ -24,22 +24,43 @@ theorem problem2 {t : ℚ} (h : t ^ 2 + t - 6 = 0) : t = 2 ∨ t = -3 := by
   calc
     (t+3)*(t-2) = t^2+t-6 := by ring
     _ = 0 := h
-  sorry
-
-
+  have h3 := eq_zero_or_eq_zero_of_mul_eq_zero h2
+  obtain h3|h3 := h3
+  right
+  addarith[h3]
+  left
+  addarith[h3]
 
 @[autograded 3]
 theorem problem3 : ∃ a b : ℕ, a ≠ 0 ∧ 2 ^ a = 5 * b + 1 := by
-  sorry
+  use 4,3
+  constructor
+  numbers
+  numbers
 
 @[autograded 5]
 theorem problem4 (x : ℚ) : ∃ y : ℚ, y ^ 2 > x := by
-  sorry
+  use x+1
+  calc
+    (x+1)^2
+    _ = x^2+2*x+1 := by ring
+    _ > x := by sorry
 
 @[autograded 5]
 theorem problem5 {x : ℕ} (hx : Odd x) : Odd (x ^ 3) := by
-  sorry
+  obtain ⟨k,hk⟩ := hx
+  use k*(3+6*k+4*k^2)
+  rw[hk]
+  ring
 
 @[autograded 5]
 theorem problem6 (n : ℕ) : ∃ m ≥ n, Odd m := by
+  use 2*n+1
+  constructor
+  have h : 2*n+1 > n :=
+  calc
+    2*n+1 = n+(n+1) := by ring
+    _ > n := by extra
   sorry
+  use n
+  ring
